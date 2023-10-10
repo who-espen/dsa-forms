4ca8e0d08ec7f344f6d6d24a28f1495f1b747c20
Dyesse YUMBA <yumba.nduba@gmail.com>
2023-10-10 19:34:03 +0100

    wip

--- ONCHO/OEM/Guinea Conakry/2023/odk_qr_code_generator.py
+++ ONCHO/OEM/Guinea Conakry/2023/odk_qr_code_generator.py
@@ -6,18 +6,19 @@ import zlib
 
 settings = {
   "general": {
-    "server_url": "https://submit.datastandard.co/espen/guinnee-conakry-espen",
-    "username": "collect",
-    "password": "espen47",
+    "server_url": "https://demo.getodk.org",
     "constraint_behavior": "on_finalize"
   },
   "admin": {
+    
+    "admin_pw": "1325",
+    "admin_login": "1325",
     "edit_saved": False,
   },
   "project": {
     "name": "QR code project",
-    "icon": "ESPEN",
-    "color": "#3399ff"
+    "icon": "Q",
+    "color": "#ff0000"
   }
 }
 
