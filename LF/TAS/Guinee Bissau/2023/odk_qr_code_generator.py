5d9f832832a08e2b97e130454478aa19b6626964
Dyesse YUMBA <yumba.nduba@gmail.com>
2024-01-26 14:56:12 +0100

    gw: add qr code

---
+++ LF/TAS/Guinee Bissau/2023/odk_qr_code_generator.py
@@ -0,0 +1,30 @@
+from base64 import b64encode
+import codecs
+import json
+import segno
+import zlib
+
+settings = {
+  "general": {
+    "server_url": "https://submit.datastandard.co/espen/guinee-bissau-espen-2",
+    "username": "collect",
+    "password": "espen47",
+  },
+  "admin": {
+   # "edit_saved": False,
+    "send_finalized": True,
+    "view_sent": True,
+    #"delete_saved": False,
+    # "admin_pw": "1325",
+  },
+  "project": {
+    "name": "Guiné-Bissau Pesquisa SCH/STH",
+    "icon": "G",
+    "color": "#ebc634"
+  }
+}
+
+qr_data = b64encode(zlib.compress(json.dumps(settings).encode("utf-8")))
+
+code = segno.make(qr_data, micro=False)
+code.save('LF/TAS/Guinee Bissau/2023/guine-bissau-settings.png', scale=5)
