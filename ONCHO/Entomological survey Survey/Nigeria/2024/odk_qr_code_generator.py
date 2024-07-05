d03aac23878bc1c642f4ab04681f3584df5d7715
Dyesse YUMBA <yumba.nduba@gmail.com>
2024-07-05 15:50:29 +0100

    ng: add ento forms

---
+++ ONCHO/Entomological survey Survey/Nigeria/2024/odk_qr_code_generator.py
@@ -0,0 +1,30 @@
+from base64 import b64encode
+import codecs
+import json
+import segno
+import zlib
+
+settings = {
+  "general": {
+    "server_url": "https://submit.datastandard.co/espen/",
+    "username": "collect",
+    "password": "espen47",
+  },
+  "admin": {
+   # "edit_saved": False,
+    "send_finalized": True,
+    "view_sent": True,
+    #"delete_saved": False,
+    # "admin_pw": "1325",
+  },
+  "project": {
+    "name": "Nigeria ONCHO",
+    "icon": "N",
+    "color": "#0066ff"
+  }
+}
+
+qr_data = b64encode(zlib.compress(json.dumps(settings).encode("utf-8")))
+
+code = segno.make(qr_data, micro=False)
+code.save('ONCHO/Entomological survey Survey/Nigeria/2024/nigeria-setting.png', scale=5)
